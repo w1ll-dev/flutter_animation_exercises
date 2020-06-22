a869f981be2b632dad1d947bdb552dbf0887568f
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-06-22 19:27:20 -0300

    gravity ball added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,16 +7,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Ticker _ticker;
+  double size = 50.0;
+  double dy = 0.5;
+  double x = 100.0;
+  double y = 100.0;
+  int gravity = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _ticker = Ticker((now) {
+      setState(() {});
+    });
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _ticker.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      child: AnimatedContainer(
-        width: size.width/10,
-        height: size.height/10,
-        color: Colors.black,
-        duration: Duration(seconds: 2),
-        
+    double height = MediaQuery.of(context).size.height;
+    if (y > height - size) {
+      dy = -dy;
+    }
+    dy += gravity;
+    y += dy;
+    return Scaffold(
+      body: Transform.translate(
+        offset: Offset(x, y),
+        child: Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
       ),
     );
   }
